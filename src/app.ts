be7a02da655a70791b8e10c56c68c7f417d326a8
Sakhawat0pu <s19hossain98@gmail.com>
2024-01-18 00:06:09 -0500

    made some changes to app.ts file

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.text());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'] })); // front-end server port
+app.use(cors({ origin: 'http://localhost:5173', credentials: true })); // front-end server port
 
 app.use('/api/v1', router);
 
